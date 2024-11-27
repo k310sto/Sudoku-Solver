59d1c5b5fd4363b689d81e257435ecfdbf26d752
k310sto <k310.sto@gmail.com>
2024-11-28 04:55:07 +0900

    11th commit

--- install_bash.sh
+++ install_bash.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-gcc ./destroyer.c -o ./destroyer
-gcc ./destroyer-f.c -o ./destroyer-f
+gcc ./solver.c -o ./SdS
+gcc ./solver_f.c -o ./SdSF
 cd ~
-if find -name "Sudoku-Destroyer" | grep -q "Sudoku-Destroyer"; then
-	mv ~/Sudoku-Destroyer ~/.Sudoku-Destroyer
-else echo "名前変更済"
+if find -name "Sudoku-Solver" | grep -q "Sudoku-Solver"; then
+	mv ~/Sudoku-Solver ~/.Sudoku-Solver
 fi
-if ! grep -q "export PATH+=:~/.Sudoku-Destroyer/" "./.bashrc"; then
-	echo "export PATH+=:~/.Sudoku-Destroyer/" >> ~/.bashrc;
-else echo "PATH設定済"
+if ! grep -q "export PATH+=:~/.Sudoku-Solver/" "./.bashrc"; then
+	echo "export PATH+=:~/.Sudoku-Solver/" >> ~/.bashrc;
 fi
 bash
 
