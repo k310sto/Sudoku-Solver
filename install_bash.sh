061d8f0ce8da5fb0274b1a9d814adcd300b4b675
k310sto <k310.sto@gmail.com>
2024-11-28 00:11:44 +0000

    13th commit

--- install_bash.sh
+++ install_bash.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-gcc ./solver.c -o ./SdS
-gcc ./solver_f.c -o ./SdSF
+gcc solver.c -o SDS
 cd ~
-if find -name "Sudoku-Solver" | grep -q "Sudoku-Solver"; then
+if find -name "Sudoku-Solver" | grep -q "~/Sudoku-Solver"; then
 	mv ~/Sudoku-Solver ~/.Sudoku-Solver
 fi
 if ! grep -q "export PATH+=:~/.Sudoku-Solver/" "./.bashrc"; then
