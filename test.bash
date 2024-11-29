9a9cb6460f1ebfdd44cbb682c69e5e9270c544f1
k310sto <k310.sto@gmail.com>
2024-11-29 14:14:21 +0000

    commit dev

--- test.bash
+++ test.bash
@@ -2,10 +2,10 @@
 res=0
 bash install_bash.sh
 res=$?
-[ "${res}" = 1 ] && echo "install error" && exit $?
+[ "${res}" = 1 ] && echo "install error" && exit ${res}
 cat test_input.txt | SDS
 res=$?
-[ "${res}" = 1 ] && echo "SDS error" && exit $?
+[ "${res}" = 1 ] && echo "SDS error" && exit ${res}
 bash uninstall_bash.sh
 res=$?
 [ "${res}" = 1 ] && echo "uninstall error"
